fba6ac8e3aff2d7d964dec7b3f44d3693dc3ae50
Paras Chauhan <paraschauhan066@gmail.com>
2025-04-28 13:39:15 +0530

    Animation set uo

--- Assets/Scripts/Player/PlayerCollisionHandler.cs
+++ Assets/Scripts/Player/PlayerCollisionHandler.cs
@@ -1,9 +1,23 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
+    [SerializeField] Animator animator;
+    [SerializeField] float collisionCooldown = 1f;
+    const string ANIMATOR_TRIGGER_HIT = "Hit";
+    float cooldownTimer = 0f;
+
+    void Update()
+    {
+        cooldownTimer += Time.deltaTime;//CooldownTimer = CooldownTimer + Time.deltaTime;
+    }
+    
     void OnCollisionEnter(Collision other)
     {
-        Debug.Log(other.gameObject.name);
+        if (cooldownTimer < collisionCooldown) return;
+
+        animator.SetTrigger(ANIMATOR_TRIGGER_HIT);
+        cooldownTimer = 0f;
     }
 }
