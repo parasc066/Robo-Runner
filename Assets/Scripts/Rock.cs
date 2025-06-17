84238a0331ac27609fbf0dcb7fc9cfcae0b66777
Paras Chauhan <paraschauhan066@gmail.com>
2025-06-17 10:26:31 +0530

    Add Rock script to implement shake effect on collision

---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,22 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    [SerializeField] float shakeModifer = 10f;
+    CinemachineImpulseSource cinemachineImpulseSource;
+
+    void Awake()
+    {
+        cinemachineImpulseSource = GetComponent<CinemachineImpulseSource>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        float distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        float shakeIntensity = (1f / distance) * shakeModifer;
+        shakeIntensity = Mathf.Min(shakeIntensity, 1f);
+
+        cinemachineImpulseSource.GenerateImpulse(shakeIntensity);
+    }
+}
