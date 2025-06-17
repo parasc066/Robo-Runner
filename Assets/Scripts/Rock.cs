a4dc44aced965e9ce17c1e39a0ea081c9a875db2
Paras Chauhan <paraschauhan066@gmail.com>
2025-06-17 12:09:43 +0530

    Enhance Rock behavior with collision effects and cooldown management

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -3,20 +3,47 @@ using UnityEngine;
 
 public class Rock : MonoBehaviour
 {
+    [SerializeField] AudioSource boulderSmashAudioSource;
+    [SerializeField] ParticleSystem collisionParticleSystem;
     [SerializeField] float shakeModifer = 10f;
+    [SerializeField] float collisionCooldown = 1f;
+
     CinemachineImpulseSource cinemachineImpulseSource;
+
+    float collisionTimer = 0f;
 
     void Awake()
     {
         cinemachineImpulseSource = GetComponent<CinemachineImpulseSource>();
     }
 
+    void Update()
+    {
+        collisionTimer += Time.deltaTime;
+    }
+
     void OnCollisionEnter(Collision other)
+    {
+        if (collisionTimer < collisionCooldown) return;
+        FireImpulse();
+        CollisionFX(other);
+        collisionTimer = 0f;
+    }
+
+    private void FireImpulse()
     {
         float distance = Vector3.Distance(transform.position, Camera.main.transform.position);
         float shakeIntensity = (1f / distance) * shakeModifer;
         shakeIntensity = Mathf.Min(shakeIntensity, 1f);
 
         cinemachineImpulseSource.GenerateImpulse(shakeIntensity);
+    }
+
+    void CollisionFX(Collision other)
+    {
+        ContactPoint contactPoint = other.contacts[0];
+        collisionParticleSystem.transform.position = contactPoint.point;
+        collisionParticleSystem.Play();
+        boulderSmashAudioSource.Play();
     }
 }
